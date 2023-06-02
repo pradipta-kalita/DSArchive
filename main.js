170fceaa01eb1320ce4abebdcb705846bd8824f0
pradipta-kalita <contact.pradiptakalita@gmail.com>
2023-06-02 21:48:54 +0530

    Adding searchAlgorithms

---
+++ main.js
@@ -0,0 +1,3 @@
+const { binarySearch } = require("./SearchAlgorithms/binarySearch");
+const { linearSearch } = require("./SearchAlgorithms/linearSearch");
+const { substringCount } = require("./SearchAlgorithms/substringCount");
