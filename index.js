ab30ad7e1589cc8c0136a637b69fd842db275632
pradipta-kalita <contact.pradiptakalita@gmail.com>
2023-06-03 16:32:34 +0530

    Adding sorting algorithms

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+//searching algorithms
 const { binarySearch } = require("./SearchAlgorithms/binarySearch");
 const { linearSearch } = require("./SearchAlgorithms/linearSearch");
 const { substringCount } = require("./SearchAlgorithms/substringCount");
+
+//sorting algorithms
+const { bubbleSort } = require("./SortingAlgorithms/bubbleSort");
+const { insertionSort } = require("./SortingAlgorithms/insertionSort");
+const { mergeSort } = require("./SortingAlgorithms/mergeSort");
+const { quickSort } = require("./SortingAlgorithms/quickSort");
+const { radixSort } = require("./SortingAlgorithms/radixSort");
+const { selectionSort } = require("./SortingAlgorithms/selectionSort");
